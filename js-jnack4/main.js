9444fb95067d4ead476f08ef385f9569402e9124
FrancescoDale <fd060862@gmail.com>
2020-10-15 16:28:51 +0200

    inserisce il numero dispari nell'array

--- js-jnack4/main.js
+++ js-jnack4/main.js
@@ -5,6 +5,6 @@ var inputNumber;
 for (i = 6; i > 0; i--) {
     inputNumber = parseFloat(prompt('inserisci un numero : '));
     if ((inputNumber % 2) != 0 ) {
-        
+        arrOdds.push(inputNumber);
     }
 }
