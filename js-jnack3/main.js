6c44ac73844009cc16acfe01f4afecc0f941dc08
FrancescoDale <fd060862@gmail.com>
2020-10-15 15:33:59 +0200

    creato un ciclo for che chiede di inserire un numero 5 volte

---
+++ js-jnack3/main.js
@@ -0,0 +1,10 @@
+var i;
+var inputNumber;
+var somma = 0;
+
+
+// imposto un ciclo for che chiede 5 volte di inserire un numero
+
+for (i = 5; i > 0; i--) {
+    inputNumber = prompt('inserisci un numero : ')
+}
