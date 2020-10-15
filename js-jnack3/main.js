64acfa392a08522458148434f5a90859e06eb387
FrancescoDale <fd060862@gmail.com>
2020-10-15 16:03:25 +0200

    aggiunto nel ciclo for l'incremento della var somma

--- js-jnack3/main.js
+++ js-jnack3/main.js
@@ -6,5 +6,8 @@ var somma = 0;
 // imposto un ciclo for che chiede 5 volte di inserire un numero
 
 for (i = 5; i > 0; i--) {
-    inputNumber = prompt('inserisci un numero : ')
+    inputNumber = prompt('inserisci un numero : ');
+    somma = ( somma + inputNumber );
 }
+
+document.getElementById('sum').innerHTML = somma;
