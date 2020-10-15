5a963c9b8f180e3d480516e3dbaad265e080e940
FrancescoDale <fd060862@gmail.com>
2020-10-15 16:04:45 +0200

    aggiunto il parseFloat() per evitare di avere come risultato la sequenza dei numeri inseriti

--- js-jnack3/main.js
+++ js-jnack3/main.js
@@ -6,7 +6,7 @@ var somma = 0;
 // imposto un ciclo for che chiede 5 volte di inserire un numero
 
 for (i = 5; i > 0; i--) {
-    inputNumber = prompt('inserisci un numero : ');
+    inputNumber = parseFloat(prompt('inserisci un numero : '));
     somma = ( somma + inputNumber );
 }
 
